c32a3599aab2d8818c6e6f8c9d31312384ebf7e0
lferreira <lferreira@atcan.es>
2015-06-16 08:54:17 +0100

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -33,11 +33,18 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 					}
 					  );
 
-//Importar definicion de la tabla Quiz en quiz.js
+// Importar definicion de la tabla Quiz en quiz.js
 //var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+// Importar definición de la tabla Comment
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definicion tabla Quiz
+exports.Comment = Comment; 
 
 //sequelize.sync() crea e inicializa tabla preguntas
 sequelize.sync().then(function() {
